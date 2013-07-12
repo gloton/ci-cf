8be37d4ba8b02d0d356a538de5fd530e9da236e3
gloton <gloton@gmail.com>
2013-07-12 14:59:01 -0400

    Clase 5 Creando el primier controlador lo llamo de la forma http://localhost/ci-cf/index.php/codigofacilito

---
+++ application/views/codigofacilito/bienvenido.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8" />
+	<title>Bienvenido</title>
+</head>
+<body>
+	<h1>Llamado desde el controlador Codigofacilito</h1>
+</body>
+</html>
