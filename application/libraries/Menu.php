75b1e1f181927f1b918bd70c691ff5f30abd7e8e
gloton <gloton@gmail.com>
2013-07-12 15:45:00 -0400

    Clase 8 - viendo la creacion de Librerias No alcance a ver el video completo porque se pego el notebook

---
+++ application/libraries/Menu.php
@@ -0,0 +1,11 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+	class Menu {
+		private $arr_menu;
+		public function __construct($arr) {
+			$this->$arr_menu = $arr;
+		}
+		public function construirMenu () {
+			$
+		}
+	} 
+?>
